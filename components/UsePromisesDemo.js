9856eb59b9d4caf4b287c4aa74439857896e1b28
indahtheresia <indahtheresiatheh29@gmail.com>
2025-02-11 12:23:16 +0800

    The use() hook in action

--- components/UsePromisesDemo.js
+++ components/UsePromisesDemo.js
@@ -1,15 +1,20 @@
-import fs from "node:fs/promises";
+'use client';
 
-export default async function UsePromisesDemo() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  const data = await fs.readFile("dummy-db.json", "utf-8");
-  const users = JSON.parse(data);
+import { useState, use } from "react";
+
+export default function UsePromisesDemo({ usersPromise }) {
+  const users = use(usersPromise);
+  const [count, setCount] = useState(0);
 
   return (
     <div className="rsc">
       <h2>RSC with Data Fetching</h2>
       <p>
         Uses <strong>async / await</strong> for data fetching.
+      </p>
+      <p>
+        <button onClick={() => setCount((prevCount) => prevCount + 1)}>Increment</button>
+        <span>{count}</span>
       </p>
       <ul>
         {users.map((user) => (
